195c151c7879fb6ee756f0b9eb469055750562cc
Mateus Almeida <Mateus.silva444@outlook.com>
2021-07-24 07:06:15 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Fbs
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			int x = 0;
+			int y = 1;
+			int z;
+			int contador = 0;
+			for (contador = 0; contador < 40; contador++)
+			{
+				z = x + y;
+				y = x;
+				x = z;
+				Console.WriteLine(z);
+
+				contador++;
+			}
+			
+		}
+	}
+}
